278ea73ef67377c8088724224dae265b3f2cd9d9
HunterWarburton <warburtonhunter@gmail.com>
2022-06-30 10:06:50 -0700

    Update GoogAuth.js
    github secrets

--- GoogAuth.js
+++ GoogAuth.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
+require('dotenv').config();
 
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
+const GOOGLE_CLIENT_ID = process.env.GOOG_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOG_CLIENT_SECRET;
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
