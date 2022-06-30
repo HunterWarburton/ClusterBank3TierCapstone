282c62777cdb46a21064184a50f1fe5cf3398e28
HunterWarburton <warburtonhunter@gmail.com>
2022-06-30 09:55:49 -0700

    Update GoogAuth.js

--- GoogAuth.js
+++ GoogAuth.js
@@ -2,8 +2,6 @@ const passport = require('passport');
 
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
-const GOOGLE_CLIENT_ID = '595295551043-45qsecbu7sjak6l9jg2efm3junopj33k.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
